c4e15468e86052d6cbcb174cd0ab063a7e3ea7ea
Frank <akura11.tt@gmail.com>
2016-06-21 11:22:40 -0500

    Adaptaciones para ckan 2.5.2

--- ckanext/feedcontent/auth.py
+++ ckanext/feedcontent/auth.py
@@ -1,5 +1,5 @@
 from pylons.i18n import _
-from ckan.authz import Authorizer
+from ckan.authz import is_sysadmin
 from ckan.logic import check_access_old
 from ckan.logic.auth import get_group_object, get_package_object
 from ckan.logic.auth.publisher import _groups_intersect
@@ -13,7 +13,7 @@ def feed_create(context, data_dict):
     model = context['model']
     user = context.get('user','')
 
-    if not Authorizer().is_sysadmin(unicode(user)):
+    if not is_sysadmin(unicode(user)):
         return {'success': False,
                 'msg': _('You are not allowed to create a new feed')}
 
@@ -25,7 +25,7 @@ def feed_get(context, data_dict):
     Feed get permissions.  Only sysadmins are allowed to view feeds
     """
     user = context.get('user','')
-    if not Authorizer().is_sysadmin(unicode(user)):
+    if not is_sysadmin(unicode(user)):
         return {'success': False,
                 'msg': _('You are not allowed to view this feed')}
     return { 'success': True }
@@ -38,7 +38,7 @@ def feed_list(context, data_dict):
     model = context['model']
     user = context.get('user','')
 
-    if not Authorizer().is_sysadmin(unicode(user)):
+    if not is_sysadmin(unicode(user)):
         return {'success': False,
                 'msg': _('You are not allowed to list feeds')}
 
@@ -52,7 +52,7 @@ def feed_update(context, data_dict):
     model = context['model']
     user = context.get('user','')
 
-    if not Authorizer().is_sysadmin(unicode(user)):
+    if not is_sysadmin(unicode(user)):
         return {'success': False,
                 'msg': _('You are not allowed to update a feed')}
 
@@ -65,7 +65,7 @@ def feed_delete(context, data_dict):
     model = context['model']
     user = context.get('user','')
 
-    if not Authorizer().is_sysadmin(unicode(user)):
+    if not is_sysadmin(unicode(user)):
         return {'success': False,
                 'msg': _('You are not allowed to delete this feed')}
 
